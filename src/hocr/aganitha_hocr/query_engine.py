47ef13ae83633ecc497de413f01a52bb621eceaa
EngineerReversed <abhishek.shakya@aganitha.ai>
2020-11-03 14:03:54 +0530

    Updated logic for compiled query and step

--- src/hocr/aganitha_hocr/query_engine.py
+++ src/hocr/aganitha_hocr/query_engine.py
@@ -1,7 +1,7 @@
 from typing import List, Union, Any, TypeVar, Optional
 from aganitha_parsing_utils.html import HTMLParsingUtils
-from src.hocr.aganitha_hocr.object_model import Region, Block
-from src.hocr.aganitha_hocr.step import Step
+from src.hocr.aganitha_hocr.object_model import Region, Block, HOCRDoc
+from src.hocr.aganitha_hocr.step import Step, StepTop, StepBottom, StepRight, StepLeft, StepText
 
 # TypeVar
 CompiledQuery_t = TypeVar('CompiledQuery')
@@ -14,11 +14,21 @@ class QueryCompiler(object):
     def compile(self) -> CompiledQuery_t:
         query_list = self.query.split('/')
         print(query_list)
-        scope = query_list[0]
         compiled_query = []
         for query in query_list[1:]:
             axis, argument = query.split(':')
-            compiled_query.append(Step(axis, argument[:-1]))
+            axis = axis.lower().strip()
+            if axis == 'top':
+                step = StepTop(axis, argument[:-1])
+            elif axis == 'bot':
+                step = StepBottom(axis, argument[:-1])
+            elif axis == 'right':
+                step = StepRight(axis, argument[:-1])
+            elif axis == 'left':
+                step = StepLeft(axis, argument[:-1])
+            elif axis == 'text':
+                step = StepText(axis, argument[:-1])
+            compiled_query.append(step)
         return compiled_query
 
 
@@ -26,23 +36,13 @@ class CompiledQuery(object):
     def __init__(self, parsed_query: List[Step]):
         self.parsed_query = parsed_query
 
-    def execute_query(self, doc: HTMLParsingUtils, context: Optional[Union[Region, Block]]) -> Any:
-        for query in self.parsed_query:
-            # query.axis , query.argument
-            if query.axis == 'TOP':
-                print('calling StepTOP()')
-            elif query.axis == 'BOT':
-                print('calling StepBOT')
-            elif query.axis == 'RIGHT':
-                print('calling StepRIGHT')
-            elif query.axis == 'LEFT':
-                print('calling StepLEFT')
-            elif query.axis == 'TEXT':
-                print('calling StepTEXT')
-        pass
+    def execute_query(self, context: Union[HOCRDoc, Region, Block]) -> Any:
+        next_step_context = context
+        for step in self.parsed_query:
+            next_step_context = step.execute(next_step_context)
+
+        return next_step_context
+
 
 cq = QueryCompiler('PAGE/TOP:20%/RIGHT:30%/TEXT:Date')
 compiled = cq.compile()
-
-
-
--- src/hocr/aganitha_hocr/step.py
+++ src/hocr/aganitha_hocr/step.py
@@ -1,7 +1,7 @@
 from src.hocr.aganitha_hocr.object_model import Region
 from src.hocr.aganitha_hocr.object_model import BlockSet
 from src.hocr.aganitha_hocr.object_model import Block
-
+from src.hocr.aganitha_hocr.object_model import HOCRDoc
 
 from typing import Union
 
@@ -9,17 +9,58 @@ from typing import Union
 class Step(object):
     def __init__(self, axis: str, argument: str):
         self.axis = axis
-        # Parsing percentage
+        # ToDO: Parsing percentage from argument
         self.argument = argument
 
-    def execute(self, context: Region) -> Union[Region, BlockSet, Block]:
+    def execute(self, context: Union[HOCRDoc, Region, Block]) -> Union[Region, BlockSet, Block]:
         pass
 
 
-class StepTOP(Step):
-    def execute(self, context: Region) -> Union[Region, BlockSet, Block]:
+class StepTop(Step):
+    def execute(self, context: Union[HOCRDoc, Region, Block]) -> Union[Region, BlockSet, Block]:
         """
         Get a new region >> 20% of top or 20% * page y value.
         return Region(x_top)
+        """
+        if not context.parent_doc:
+            hocr = context.hocr
+        else:
+            hocr = context.parent_doc
+
+        return
+
+
+class StepBottom(Step):
+    def execute(self, context: Union[HOCRDoc, Region, Block]) -> Union[Region, BlockSet, Block]:
+        """
+        Get a new region >> 20% of bottom or 20% * page y value.
+        return Region(x_bottom)
+        """
+        return
+
+
+class StepLeft(Step):
+    def execute(self, context: Union[HOCRDoc, Region, Block]) -> Union[Region, BlockSet, Block]:
+        """
+        Get a new region >> 20% of left or 20% * page x value.
+        return Region(y_left)
+        """
+        return
+
+
+class StepRight(Step):
+    def execute(self, context: Union[HOCRDoc, Region, Block]) -> Union[Region, BlockSet, Block]:
+        """
+        Get a new region >> 20% of right or 20% * page x value.
+        return Region(y_right)
+        """
+        return
+
+
+class StepText(Step):
+    def execute(self, context: Union[HOCRDoc, Region, Block]) -> Union[Region, BlockSet, Block]:
+        """
+        Get text from the region
+        return Block
         """
         return
