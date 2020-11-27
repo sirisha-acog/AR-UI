b91e5e6a84f60728a3c74ed694bb756848e9f97f
Adarsh Das <adarsh@pop-os.localdomain>
2020-11-27 08:16:38 +0530

    Updated for Katz

--- src/hocr/aganitha_hocr/controller.py
+++ src/hocr/aganitha_hocr/controller.py
@@ -4,6 +4,7 @@ from src.hocr.aganitha_hocr.template_repo.GroupM import GroupM
 from src.hocr.aganitha_hocr.template_repo.OMG_USA_Media import OMG
 from src.hocr.aganitha_hocr.template_repo.Squared import Squared
 from src.hocr.aganitha_hocr.template_repo.IPG import IPG
+from src.hocr.aganitha_hocr.template_repo.Katz_Media_Group import Katz
 import logging
 import json
 import os
@@ -12,8 +13,8 @@ import pandas as pd
 logger = logging.getLogger(__name__)
 
 if __name__ == "__main__":
-    page = Page('/home/adarsh/work/ar-automation/tmp/07.06.20-lb83197-1-10-addl-doc-01.jpg.hocr.hocrjs.html')
-    template = IPG()
+    page = Page('/home/adarsh/work/ar-automation/tmp/09.24.20-lb83199-1-11-addl-doc-01.jpg.hocr.hocrjs.html')
+    template = Katz()
     extracted_values = template.execute(page.page_blockset)
     print(extracted_values)
     # extracted_values.to_csv('/home/adarsh/work/ar-automation/output/csv/22squared_1.csv', index=False)
--- src/hocr/aganitha_hocr/template_repo/Katz_Media_Group.py
+++ src/hocr/aganitha_hocr/template_repo/Katz_Media_Group.py
@@ -16,6 +16,48 @@ logger = logging.getLogger(__name__)
 
 
 # PREDICATES -->
+class TopLeftCustomerNameChecker(Predicate):
+    def check(self, context: BlockSet) -> bool:
+        block_set = get_text(context, named_params={'query': self.anchor,
+                                                    'level': "phrase"})
+        block = block_set.get_synthetic_block()
+        if len(block.word.split()) == 3:
+            return True
+        else:
+            return False
+
+
+class TopRightPayeeChecker(Predicate):
+    def check(self, context: BlockSet) -> bool:
+        block_set = get_text(context, named_params={'query': self.anchor,
+                                                    'level': "word"})
+        block = block_set.get_synthetic_block()
+        if len(block.word.split()) == 1:
+            return True
+        else:
+            return False
+
+
+class TopLeftStationNameChecker(Predicate):
+    def check(self, context: BlockSet) -> bool:
+        block_set = get_text(context, named_params={'query': self.anchor,
+                                                    'level': "word"})
+        block = block_set.get_synthetic_block()
+        if len(block.word.split()) == 1:
+            return True
+        else:
+            return False
+
+
+class TopLeftStationInvoiceChecker(Predicate):
+    def check(self, context: BlockSet) -> bool:
+        block_set = get_text(context, named_params={'query': self.anchor,
+                                                    'level': "word"})
+        block = block_set.get_synthetic_block()
+        if len(block.word.split()) == 1:
+            return True
+        else:
+            return False
 
 # MATCHERS -->
 
@@ -24,15 +66,46 @@ logger = logging.getLogger(__name__)
 class Katz(Extractor):
 
     def __init__(self):
-        self.date: BlockSet = None
-        self.check_number: BlockSet = None
-        self.invoice_date: BlockSet = None
-        self.invoice_number: BlockSet = None
-        self.amount: BlockSet = None
-        self.check_total: BlockSet = None
+        self.customer_name: BlockSet = None
+        self.payee: BlockSet = None
+        # Table Headers
+        self.station: BlockSet = None
+        self.station_inv: BlockSet = None
+        self.bcst_mth: BlockSet = None
+        self.voucher: BlockSet = None
+        self.grs_order: BlockSet = None
+        self.grs_billed: BlockSet = None
+        self.description: BlockSet = None
+        self.paid_amount: BlockSet = None
 
     def match(self, context: BlockSet) -> bool:
-        pass
+        status_list = []
+        # Customer Name
+        context_customer_name = left(top(context, named_params={'argument': 30}), named_params={'argument': 40})
+        if TopLeftCustomerNameChecker(anchor='Katz Media Group').check(context_customer_name):
+            self.customer_name = context_customer_name
+        status_list.append(TopLeftCustomerNameChecker(anchor='Katz Media Group').check(context_customer_name))
 
-    def extract(self, context:BlockSet) -> Dict:
-        pass
+        # Payee Check
+        context_payee = right(top(context, named_params={'argument': 30}), named_params={'argument': 40})
+        if TopRightPayeeChecker(anchor='Payee:').check(context_payee):
+            self.payee = context_payee
+        status_list.append(TopRightPayeeChecker(anchor='Payee:').check(context_payee))
+
+        # Station Check
+        context_station = left(top(context, named_params={'argument': 70}), named_params={'argument': 40})
+        if TopLeftStationNameChecker(anchor='Station').check(context_station):
+            self.station = context_station
+        status_list.append(TopLeftStationNameChecker(anchor='Station').check(context_station))
+
+        # StationInvoice Check
+        context_station_inv = left(top(context, named_params={'argument': 70}), named_params={'argument': 40})
+        if TopLeftStationInvoiceChecker(anchor='Stn-Invoice').check(context_station_inv):
+            self.station_inv = context_station_inv
+        status_list.append(TopLeftStationInvoiceChecker(anchor='Stn-Invoice').check(context_station_inv))
+        print(status_list)
+        return True
+
+    def extract(self, context: BlockSet) -> Dict:
+        extracted_params = {}
+        return extracted_params
